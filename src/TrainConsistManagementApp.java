b095c466e5062fb1abe701d0daf329398b802422
gopavaram2006-ctrl <gopavaram2006@gmail.com>
2026-03-24 08:38:16 +0530

    Base application setup

---
+++ src/TrainConsistManagementApp.java
@@ -0,0 +1,8 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class TrainConsistManagementApp {
+    public static void main(String[] args) {
+        // UC1: Initialize Train and Display Consist Summary
+    }
+}
